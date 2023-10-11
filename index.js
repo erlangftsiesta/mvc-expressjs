35437525748e2e5b47b7f2ff0544dd8b9e7f4f5d
erlangftsiesta12 <erlangftsiesta12@gmail.com>
2023-10-11 16:22:50 +0700

    Nambahin Show Data tapi error bejir

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const loginRoutes = require('./src/routes/router-login');
 const registerRoutes = require('./src/routes/router-register');
 const appRoutes = require('./src/routes/router-app');
 const createDataRoutes = require('./src/routes/router-create-data');
+const showDataRoutes = require('./src/routes/router-show-data');
 
 // Configurasi dan gunakan library
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -38,6 +39,7 @@ app.set('view engine', 'ejs');
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
 app.use('/create-data', createDataRoutes)
+app.use('/show-data', showDataRoutes)
 app.use('/', appRoutes);
 app.use(flash());
 
